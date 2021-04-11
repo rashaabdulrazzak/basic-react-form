ca9fbefd77fa403504d4f6b1e8a997fe318d09a8
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2021-04-11 18:18:11 +0300

    basic input example

---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+ 
+class Form extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            name="firstName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.firstName}
+          />
+          <input
+            type="text"
+            name="lastName"
+            onChange={event => this.props.handleChange(event)}
+            value={this.props.formData.lastName} />
+        </form>
+      </div>
+    )
+  }
+}
+ 
+export default Form;
